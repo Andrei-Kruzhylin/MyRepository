31843df028fcfe72b7f66627052081497dd46f40
Andrei Kruzhylin <mr.andkruz@gmail.com>
2020-10-22 23:11:55 +0300

    Реализация Main

--- lab1/src/Lab1_Var8/Main.java
+++ lab1/src/Lab1_Var8/Main.java
@@ -2,7 +2,28 @@ package Lab1_Var8;
 
 public class Main {
 
-    public static void main(String[] args){
 
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) throws Exception{
+        Food[] breakfast = new Food[20];
+        int itemsSoFar = 0;
+        for (String arg: args) {
+            String[] parts = arg.split("/");
+            if (parts[0].equals("Cheese")) {
+                breakfast[itemsSoFar] = new Cheese();
+            } else
+            if (parts[0].equals("Apple")) {
+                breakfast[itemsSoFar] = new Apple(parts[1]);
+            }
+            itemsSoFar++;
+        }
+        System.out.print("Продуктов съедено: ");
+        System.out.println(itemsSoFar);
+        for (Food item: breakfast)
+            if (item!=null)
+                item.consume();
+            else
+                break;
+        System.out.println("Всего хорошего!");
     }
 }
