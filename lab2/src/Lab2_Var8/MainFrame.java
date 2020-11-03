84ee421a0e73c3438bd9daaf01135e1e658b8509
Andrei Kruzhylin <mr.andkruz@gmail.com>
2020-11-03 18:50:25 +0300

    Добавлен главный метод класса(без пункта б)

--- lab2/src/Lab2_Var8/MainFrame.java
+++ lab2/src/Lab2_Var8/MainFrame.java
@@ -142,6 +142,12 @@ public class MainFrame extends JFrame {
 
         }
 
+        
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
 }
 
 
